0c9f2aee3b602f25672062e08dc10dc4acd1ec9e
sarism <saraisabelmayalasso@gmail.com>
2024-09-25 06:49:28 -0500

    feat

---
+++ reutilizable-ts/src/components/indexPadre.ts
@@ -0,0 +1,1 @@
+export  {default as Post} from './card/card.js'
---
+++ reutilizable-ts/src/data/data.ts
@@ -0,0 +1,91 @@
+import lemonImage from '../assets/images/1.jpg'
+import toastImage from '../assets/images/2.jpg'
+import braskfastImage from '../assets/images/3.jpg'
+import toast2Image from '../assets/images/4.jpg'
+import pancakesImage from '../assets/images/5.jpg'
+import tomatoImage from '../assets/images/6.jpg'
+import tunaImage from '../assets/images/7.jpg'
+import bowlImage from '../assets/images/8.jpg'
+import berriesImage from '../assets/images/9.jpg'
+import chessecakeImage from '../assets/images/10.jpg'
+import lunchImage from '../assets/images/11.jpg'
+import pizzaImage from '../assets/images/12.jpg'
+
+export const recipes = [
+    {
+        id: 1,
+        userName: "EmilyDavis",
+        recipeName: "Lemon Cake",
+          photo: lemonImage
+    },
+    {
+        id: 2,
+        userName: "EmmaWilson",
+        recipeName: "French Toast",
+          photo: toastImage
+
+    },
+    {
+        id: 3,
+        userName: "LucasGonzalez",
+        recipeName: "Romantic Breakfast",
+        photo: braskfastImage
+
+    },
+    {
+        id: 4,
+        userName: "AmeliaHernandez",
+        recipeName: "Toast Board",
+        photo: toast2Image
+    },
+    {
+        id: 5,
+        userName: "AndrewHall",
+        recipeName: "Strawberrie Pancakes",
+        photo: pancakesImage
+    },
+    {
+        id: 6,
+        userName: "MiaKing",
+        recipeName: "Tomato Baskets",
+        photo: tomatoImage
+    },
+    {
+        id: 7,
+        userName: "SaraDavis",
+        recipeName: "Tuna Entree",
+        photo: tunaImage
+    },
+    {
+        id: 8,
+        userName: "JuliaWilson",
+        recipeName: "Healthy bowl",
+        photo: bowlImage
+    },
+    {
+        id: 9,
+        userName: "AndrewHall",
+        recipeName: "Berries cakes",
+        photo: berriesImage
+    },
+    {
+        id: 10,
+        userName: "MiaKing",
+        recipeName: "Cheesecake",
+        photo: chessecakeImage
+    },
+    {
+        id: 11,
+        userName: "SaraDavis",
+        recipeName: "Healthy Lunch",
+        photo: lunchImage
+    },
+    {
+        id: 12,
+        userName: "JuliaWilson",
+        recipeName: "Margarita Pizza",
+        photo: pizzaImage
+    }
+];
+
+export default recipes;
